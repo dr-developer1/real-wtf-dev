69c37d5de2bbf502c90f5d0d0ec4f68986ef36ef
aibrahim - Dr Developer <ami@silico.fr>
2025-05-14 22:20:19 +0200

    Add Swagger, CORS, and Docker support with environment setup
    - Enabled CORS and integrated Swagger for API documentation in development mode.
    - Added Docker Compose configuration to set up a PostgreSQL database with environment variables.
    - Updated scripts to set NODE_ENV and added an example `.env` file for easier configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: process.env.CORS_ORIGIN ?? '',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
+    if (process.env.NODE_ENV === 'development') {
+        const config = new DocumentBuilder()
+            .setTitle('NestJS Practical API Example')
+            .setDescription(
+                'A production-ready NestJS API with CORS configuration, environment variables, and Swagger documentation. Clone and use as a starting point for your own projects.',
+            )
+            .setVersion('1.0')
+            .addTag('api')
+            .build();
+        const documentFactory = () => SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api', app, documentFactory);
+    }
     await app.listen(process.env.PORT ?? 3000);
-    console.log(`Application is running on port ${process.env.PORT ?? 3000}`);
+    console.log(
+        `Application is running on port ${process.env.PORT ?? 3000} in ${process.env.NODE_ENV} mode`,
+    );
 }
 
 void bootstrap();
